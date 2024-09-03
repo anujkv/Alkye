3cab689a3b40ac5e4d30b6984e084db9216f4be5
anujkv <anujkrvaish@gmail.com>
2024-09-03 16:34:43 +0530

    instrumentation test cases added in workflow

--- app/src/androidTest/java/com/net/alkye/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/net/alkye/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@ package com.net.alkye
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.net.alkye.ui.screens.ArticleDetailScreen
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,4 +22,17 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.net.alkye", appContext.packageName)
     }
+
+    /*@Test
+    fun testNavigationFromArticleDetailScreen() {
+        val navController = TestNavHostController(context)
+        composeTestRule.setContent {
+            ArticleDetailScreen(articleId = 1, navController = navController)
+        }
+
+        composeTestRule.onNodeWithContentDescription("Logo Icon").performClick()
+        // Verify navigation
+        assertEquals(navController.currentDestination?.route, "expected_route")
+    }*/
 }
+
